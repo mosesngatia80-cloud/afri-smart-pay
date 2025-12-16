32dc8f64cb53119a3e5b7612fc01d4d16c1dc34d
Moses Ngatia <mosesngatia80@gmail.com>
2025-12-16 22:57:12 +0300

    Fix Wallet model export

--- models/Wallet.js
+++ models/Wallet.js
@@ -1,10 +1,18 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
-const walletSchema = new mongoose.Schema({
-  phone: { type: String, unique: true },
-  balance: { type: Number, default: 0 },
-  pin: { type: String },
-  transactions: { type: Array, default: [] },
-});
+const walletSchema = new mongoose.Schema(
+  {
+    phone: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    balance: {
+      type: Number,
+      default: 0,
+    },
+  },
+  { timestamps: true }
+);
 
-export default mongoose.model("Wallet", walletSchema);
+module.exports = mongoose.model("Wallet", walletSchema);
